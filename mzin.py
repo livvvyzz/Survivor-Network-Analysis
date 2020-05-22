005977f4a9814e60ff1afb71c77b09d195e98370
livvvyzz <oliviamcrobbie@hotmai.com>
2020-05-23 00:47:46 +1200

    cleaning up the array

--- mzin.py
+++ mzin.py
@@ -1,6 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
-import pandas
+import pandas as pd
 website_url = requests.get('https://survivor.fandom.com/wiki/Survivor:_China').text
 
 soup = BeautifulSoup(website_url,'lxml')
@@ -10,14 +10,29 @@ My_table = soup.find('table',{'class':'wikitable article-table'})
 #print(My_table)
 
 table_text = My_table.get_text()
-print(table_text.splitlines())
+table_array = table_text.splitlines()
 
 #clean up the table text.
+cleaned_array = table_array[14:]
 
+
+def get_next_castaway(array):
+    clean = array[0:3]
+    print(clean)
+    count = 3
+    print(clean[count-1], clean[count-2])
+    while clean[count-1] != '' and clean[count-2] != '' and clean[count-3] != '':
+        print('text')
+        clean = array[0:count]
+        count = count + 1
+    print(count)
+    return count
+
+get_next_castaway(cleaned_array)
 
 #create the table of castaway info
 column_names = ["Name", "Age", "Hometown", "Tribe", "Rank", "Day voted out", "Votes against"]
-df = pd.Dataframe(columns = column_names)
+df = pd.DataFrame(columns = column_names)
 
 ##Extract import info from the table
 ##We want Name, Tribes, Day voted out
